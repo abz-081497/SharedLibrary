68ec7c1330eb5f722cf5be8d809744ae524f3efd
root <root@BUHPL61497Q1V.dir.svc.accenture.com>
2021-10-08 13:03:50 +0300

    small change

--- vars/uploadFilesToS3.groovy
+++ vars/uploadFilesToS3.groovy
@@ -1,4 +1,4 @@
-def call(Map stageParams = [Region: "us-east-1", s3Bucket: "test-bucket-222", s3Path: "/"]) {
+def call(Map stageParams = [Region: "us-east-1", s3Bucket: "test-bucket-2221", s3Path: "/"]) {
 
     withAWS(region: stageParams.Region, credentials:'awscredentials') {
         awsIdentity()
