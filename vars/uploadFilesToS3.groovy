dac024fa55533658f264fe48ad5c458c4c5394b8
Abigael Valenzuela <87469977+abz-081497@users.noreply.github.com>
2021-10-11 20:58:11 +0800

    Update uploadFilesToS3.groovy

--- vars/uploadFilesToS3.groovy
+++ vars/uploadFilesToS3.groovy
@@ -1,4 +1,4 @@
-def call(String path = "*.jar", String s3Path = "/",String workingDir = "target", String  s3Bucket,Map stageParams = [Region: "us-east-1",  s3Bucket: "test-bucket-2221", s3Path: "/"]) {
+def call(String path = "*.jar", String s3Path = "/",String workingDir = "target", String  s3Bucket,Map stageParams = [Region: "us-east-1",  s3Bucket: "test-bucket-demo-abigael", s3Path: "/"]) {
 
     withAWS(region: stageParams.Region, credentials:'awscredentials') {
         awsIdentity()
